3a9c0e7b18d0b41fcf79d7a19c7134063dc4a9f5
psaini2 <psaini2@ncsu.edu>
2024-02-07 23:50:21 -0500

    admin authentication

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,11 @@
 class User < ApplicationRecord
+  before_create :set_defaults
   has_secure_password
   validates :email, presence: true, uniqueness: true
+
+  private
+
+  def set_defaults
+    self.is_admin ||= 0 #setting default value for users other than admin
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_02_08_033930) do
+ActiveRecord::Schema[7.1].define(version: 2024_02_08_041713) do
   create_table "users", force: :cascade do |t|
     t.string "email"
     t.string "password"
@@ -21,6 +21,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_02_08_033930) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "password_digest"
+    t.boolean "is_admin"
   end
 
 end
