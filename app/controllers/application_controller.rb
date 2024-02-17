154e40b1220512cdab72e3b858916bb5a35e06d0
Siddharth Shah <ssshah26@ncsu.edu>
2024-02-16 23:19:50 -0500

    reset

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   helper_method :current_user
   before_action :authorized
   helper_method :logged_in?
-
+  before_action :reset_session
   def current_user
     if session[:user_id]
       @current_user ||= User.find(session[:user_id])
